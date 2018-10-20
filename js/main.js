cc82ba0e6f8a803aca303762e4334e9b18e1deb6
sfongx <sleefong@gmail.com>
2018-10-20 14:35:09 -0400

    Another attempt

--- js/main.js
+++ js/main.js
@@ -3,14 +3,8 @@
 function screenCheck(){
     var screenWidth = screen.width;
 
-    // Phone screens
-    if (screenWidth <= 500) {
-        // Newline between second and third entries
-        document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/>';
-    }
-
-    // Tablet screens
-    if (screenWidth >= 501 && screenWidth <= 1000) {
+    // Tablet and phone screens screens
+    if (screenWidth <= 1000) {
         // Newline between third and fourth entries
         document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/>';
     }
