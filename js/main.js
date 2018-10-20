97ed918d8ff1e6ab757ef09ab7ec390d5bb9cf39
sfongx <sleefong@gmail.com>
2018-10-20 14:22:35 -0400

    Another mobile test...

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ function screenCheck(){
     // Phone screens
     if (screenWidth <= 500) {
         // Newline between second and third entries
-        document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/><br/>';
+        document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/>';
     }
 
     // Tablet screens
     if (screenWidth >= 501 && screenWidth <= 1000) {
         // Newline between third and fourth entries
-        document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/><br/>';
+        document.getElementById("mobile-nline1").innerHTML = '<br/><br/><br/>';
     }
 
 }
